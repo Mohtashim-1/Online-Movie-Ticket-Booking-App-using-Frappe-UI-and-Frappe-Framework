7102b7f854d4b34fe5e7480e46713c0ae40e0e15
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-04-21 16:05:19 +0000

    working with design in movie details.vue

--- frontend/src/pages/MovieDetails.vue
+++ frontend/src/pages/MovieDetails.vue
@@ -3,9 +3,12 @@
 
     <div class="">
         <h1 class=" text-gray-900 font-bold text-6xl text-[32px]">Openheimer</h1>
-        <div>
-            <div>Director</div>
-            <div>Christoper Nohan</div>
+        <div class="mt-11">
+            <div class="flex flex-col space-y-3">
+                <div class="text-gray-700 text-base-bold font-bold uppercase">Director</div>
+                <p class="text-gray-700 text-base-bold text-xl font-semibold ">Christopher Nolan</p>
+
+            </div>
         </div>
     </div>
 
